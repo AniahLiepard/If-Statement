cb7c9ae9173b4047b83877ff4fd18b9d3a7e4559
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:00:48 -0400

    If Statement Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17975

---
+++ ProjectSeven.java
@@ -0,0 +1,11 @@
+public class ProjectSeven {
+	public static void main(String args[]){
+		int yummyCandy = 16;
+		
+		if (yummyCandy <= 4){
+			System.out.println("You are not having any of my Warheads..... I have a low supply.....");
+		} else {
+			System.out.println("Would you like some Sour Patch Kids? I got plenty!");
+		}
+	}
+}
